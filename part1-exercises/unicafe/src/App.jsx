92cbb2a6c677fe719575803649fc5b995f16dacd
Cameron Cruse <132945596+cameron-cruse@users.noreply.github.com>
2024-11-22 13:26:07 -0600

    finish 1.6

--- part1-exercises/unicafe/src/App.jsx
+++ part1-exercises/unicafe/src/App.jsx
@@ -1,4 +1,34 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Feedback = ({ good, setGood, neutral, setNeutral, bad, setBad }) => {
+  return (
+    <div className="feedback">
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good"></Button>
+      <Button
+        handleClick={() => setNeutral(neutral + 1)}
+        text="neutral"
+      ></Button>
+      <Button handleClick={() => setBad(bad + 1)} text="bad"></Button>
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div className="statistics">
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,7 +36,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <>
+      <Feedback
+        good={good}
+        setGood={setGood}
+        neutral={neutral}
+        setNeutral={setNeutral}
+        bad={bad}
+        setBad={setBad}
+      ></Feedback>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+    </>
+  );
 };
 
 export default App;
